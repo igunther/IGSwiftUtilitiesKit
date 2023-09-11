b5097e79cdb5695d50db716b268ce4d9b6be01dd
Øystein Günther <github@gunther.cc>
2023-09-11 10:48:09 +0200

    Added IGKeychainManager

---
+++ Sources/IGKeychain/IGKeychainManager.swift
@@ -0,0 +1,59 @@
+//
+//  IGKeychainManager.swift
+//  
+//
+//  Created by Øystein Günther on 11/09/2023.
+//
+
+import Foundation
+import Combine
+
+/// A manager to interact with the keychain using a generic key type.
+@available(macOS 10.15, *)
+public class KeychainManager<Key: IGKeychainKey>: IGKeychainManagerProtocol {
+    
+    /// The keychain utility to save, retrieve, or delete items.
+    private var keychain: IGKeychain<Key>
+    
+    /// A Combine subject to notify about changes in the keychain.
+    let changesSubject = PassthroughSubject<(Key, String?), Never>()
+
+    /// Private initializer to ensure the KeychainManager is only instantiated using the `configure` method.
+    /// - Parameter service: The service name for the keychain. Used to differentiate between different keychain items.
+    private init(service: String) {
+        self.keychain = IGKeychain<Key>(service: service)
+    }
+    
+    /// Configures and returns an instance of the KeychainManager.
+    /// - Parameter service: The service name for the keychain.
+    /// - Returns: An instance of the KeychainManager.
+    static public func configure(with service: String) -> KeychainManager<Key> {
+        return KeychainManager(service: service)
+    }
+    
+    /// Retrieves a value from the keychain for a given key.
+    /// - Parameter key: The key to retrieve the value for.
+    /// - Returns: The stored string value, or nil if not present.
+    public func getValue(for key: Key) -> String? {
+        return try? keychain.get(for: key)
+    }
+    
+    /// Sets (or deletes) a value in the keychain for a given key.
+    /// - Parameters:
+    ///   - value: The value to store, or nil to delete.
+    ///   - key: The key to associate the value with.
+    public func setValue(_ value: String?, for key: Key) {
+        if let val = value {
+            try? keychain.save(val, for: key)
+        } else {
+            try? keychain.delete(for: key)
+        }
+        changesSubject.send((key, value))
+    }
+    
+    /// Deletes all keys and their associated values from the keychain.
+    /// - Throws: An error if the deletion process fails.
+    public func deleteAllKeys() throws {
+        try keychain.deleteAllKeys()
+    }
+}
--- Sources/IGKeychain/IGKeychainProtocols.swift
+++ Sources/IGKeychain/IGKeychainProtocols.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 public protocol IGKeychainKey {
     var rawValue: String { get }
@@ -17,5 +18,30 @@ public protocol IGKeychainProtocol {
     func save(_ value: String, for key: Key) throws
     func get(for key: Key) throws -> String?
     func delete(for key: Key) throws
+    func deleteAllKeys() throws
+}
+
+/// Protocol that outlines the required methods and properties for a keychain manager.
+protocol IGKeychainManagerProtocol {
+    
+    associatedtype Key: IGKeychainKey
+
+    /// A Combine subject that notifies about changes in the keychain.
+    @available(macOS 10.15, *)
+    var changesSubject: PassthroughSubject<(Key, String?), Never> { get }
+
+    /// Retrieves a value from the keychain for a given key.
+    /// - Parameter key: The key to retrieve the value for.
+    /// - Returns: The stored string value, or nil if not present.
+    func getValue(for key: Key) -> String?
+
+    /// Sets (or deletes) a value in the keychain for a given key.
+    /// - Parameters:
+    ///   - value: The value to store, or nil to delete.
+    ///   - key: The key to associate the value with.
+    func setValue(_ value: String?, for key: Key)
+
+    /// Deletes all keys and their associated values from the keychain.
+    /// - Throws: An error if the deletion process fails.
     func deleteAllKeys() throws
 }
